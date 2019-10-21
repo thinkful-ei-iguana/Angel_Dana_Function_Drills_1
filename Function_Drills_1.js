07cbb1c0432f5ef8e32826774c0b0ce9a2027c91
Angel Alicea <ala31996@gmail.com>
2019-10-21 16:38:06 -0400

    added getYearOfBirth

--- Function_Drills_1.js
+++ Function_Drills_1.js
@@ -1,8 +1,13 @@
 // eslint-disable-next-line strict
+function getYearOfBirth(age){
+  return new Date().getFullYear() - age;
+}
+
 function createGreeting (name, age){
-  const yearOfBirth = new Date().getFullYear() - age;
-  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}`;
+  const yearOfBirth = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting('Angel', 23);
 console.log(greeting1);
+
