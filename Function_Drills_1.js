453d8b4573ec68772d5aec35b0f8de7b92211b4e
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-10-21 13:53:50 -0700

    added error handling for negative ages

--- Function_Drills_1.js
+++ Function_Drills_1.js
@@ -1,13 +1,21 @@
 // eslint-disable-next-line strict
 function getYearOfBirth(age){
-  return new Date().getFullYear() - age;
+  if (age<0){throw new Error ('Age cannot be negative');}
+  else {
+    return new Date().getFullYear() - age;
+  }
 }
 
 function createGreeting (name, age){
   const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.`;
 }
+try{
+  const greeting1 = createGreeting('Angel', 23);
+  console.log(greeting1);
+} 
+catch(e){
+  console.log(e);
+}
 
-const greeting1 = createGreeting('Angel', 23);
-console.log(greeting1);
 
