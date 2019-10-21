0ea1bccc4579dfb1fad505022e21592291bdc1aa
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-10-21 13:33:05 -0700

    fixed yearOfBirth. added sentence to return statement

--- Function_Drills_1.js
+++ Function_Drills_1.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line strict
 function createGreeting (name, age){
-  const yearOfBirth = Date.now().getFullYear() - age;
-  return `Hi, my name is ${name} and I am ${age} years old.`;
+  const yearOfBirth = new Date().getFullYear() - age;
+  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}`;
 }
 
 const greeting1 = createGreeting('Angel', 23);
